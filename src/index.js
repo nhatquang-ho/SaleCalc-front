cbcb6c60311ec788c4d7b16694d416dcee85170c
Nhat Quang HO <72070122+nhatquang-ho@users.noreply.github.com>
2025-01-11 13:41:33 +0100

    add files and packages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
+import { ThemeProvider, createTheme } from '@mui/material/styles'; // Import ThemeProvider and createTheme
+
+const theme = createTheme(); // Create a default theme
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ThemeProvider theme={theme}> {/* Wrap with ThemeProvider */}
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
