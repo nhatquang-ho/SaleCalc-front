44ebf2a1d3bf8e42d62028d526221abed89908b8
Nhat Quang HO <72070122+nhatquang-ho@users.noreply.github.com>
2025-03-11 23:11:29 +0100

    test axios

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,13 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import NavBar from '../NavBar/NavBar'
 import ShopPage from '../ShopPage/ShopPage'
 import TransactionPage from '../TransactionPage/TransactionPage'
 import StatisticsPage from '../StatisticsPage/StatisticsPage'
 import OptionsPage from '../OptionsPage/OptionsPage'
 import { MainContent } from './MainPage.styles'
+import { fetchData } from '../../data/commandRequests'
+import { KeycloakContext } from '../../auth/KeycloakProvider';
 
 function MainPage() {
     const [currentPage, setCurrentPage] = useState(0) // 0: Shop, 1: Transaction, 2: Statistics, 3: Options
+    const keycloak = useContext(KeycloakContext);
+
+    useEffect(() => {
+        if (keycloak !== undefined) {
+            console.log('Keycloak:', keycloak.keycloak.token);
+
+            fetchData('http://localhost:9090/api/commands/', {
+                'Authorization': `Bearer ${keycloak.keycloak.token}`
+            })
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        }
+    }, [keycloak])
 
     return (
         <div>
